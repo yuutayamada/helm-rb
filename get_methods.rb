71f87745c4983bcf9c70d8b16b20d84349eb1e27
Yuta Yamada <cokesboy@gmail.com>
2013-09-15 03:47:23 -0400

    Add literals

--- get_methods.rb
+++ get_methods.rb
@@ -2,11 +2,22 @@
 # -*- coding: utf-8 :mode ruby-mode -*-
 
 def print_methods()
+  def pretty_print(klass, literal)
+    klass.methods.each do |method|
+      class_name = literal ? klass.class.to_s : klass.to_s
+      puts class_name + "#" + method.to_s
+    end
+  end
+
   Class.constants.select do |c|
     if Class.const_get(c).is_a? Class
       klass = Class.const_get(c)
-      klass.methods.each {|method| puts klass.to_s + "#" + method.to_s}
+      pretty_print(klass, nil)
     end
+  end
+
+  ["", 0, //, [], {}].each do |literal|
+    pretty_print(literal, true)
   end
 end
 
