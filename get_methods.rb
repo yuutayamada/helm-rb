7c065df73fa2846b2a2c1662d0cf0e2d2f813382
Yuta Yamada <cokesboy@gmail.com>
2013-09-15 15:33:59 -0400

    Collect other classes/modules

--- get_methods.rb
+++ get_methods.rb
@@ -17,13 +17,32 @@ def print_methods()
     end
   end
 
-  Module.constants.select do |c|
-    klass = Module.const_get(c)
-    if klass.class == Class || klass.class == Module
+  def method_missing(name)
+    # do nothing if instance_methods method wasn't exist
+    nil
+  end
+
+  def print_modules
+    Module.constants.select do |c|
+      klass = Module.const_get(c)
+      if klass.class == Class || klass.class == Module
+        pretty_print(klass)
+      end
+    end
+  end
+
+  def print_other_modules
+    `ri -l --no-pager`.split.each do |klass|
       pretty_print(klass)
     end
   end
 
+  begin
+    print_modules
+    print_other_modules
+  rescue Exception => e
+    puts e
+  end
 end
 
 print_methods
