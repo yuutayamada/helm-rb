8c38c84f85a8c0799ba39640f719b985f96b2f1e
Yuta Yamada <cokesboy@gmail.com>
2013-09-15 16:35:03 -0400

    Use anonymous functions

--- get_methods.rb
+++ get_methods.rb
@@ -1,48 +1,39 @@
 #!/usr/bin/env ruby
 # -*- coding: utf-8 :mode ruby-mode -*-
 
-def print_methods()
-
-  def pretty_print(klass)
-    def print(klass, method)
-      puts klass.to_s + "#" + method.to_s
-    end
-
-    klass.methods.each do |method|
-      print(klass, method)
-    end
-
-    klass.instance_methods.each do |method|
-      print(klass, method)
-    end
-  end
-
+print_methods = lambda do
   def method_missing(name)
     # do nothing if instance_methods method wasn't exist
     nil
   end
 
-  def print_modules
-    Module.constants.select do |c|
-      klass = Module.const_get(c)
-      if klass.class == Class || klass.class == Module
-        pretty_print(klass)
+  begin
+    pretty_print = lambda do |klass|
+      print = lambda {|klass, method| puts klass.to_s + "#" + method.to_s}
+      klass.methods.each {|method| print.call(klass, method)}
+      klass.instance_methods.each {|method| print.call(klass, method)}
+    end
+
+    print_modules = lambda do
+      Module.constants.select do |c|
+        klass = Module.const_get(c)
+        if klass.class == Class || klass.class == Module
+          pretty_print.call(klass)
+        end
       end
     end
-  end
 
-  def print_other_modules
-    `ri -l --no-pager`.split.each do |klass|
-      pretty_print(klass)
+    print_other_modules = lambda do
+      `ri -l --no-pager`.split.each do |klass|
+        pretty_print.call(klass)
+      end
     end
-  end
 
-  begin
-    print_modules
-    print_other_modules
+    print_modules.call
+    print_other_modules.call
   rescue Exception => e
     puts e
   end
 end
 
-print_methods
+print_methods.call
