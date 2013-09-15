22cef55b15e2a9d93710ba4411adcddc1f8785b4
Yuta Yamada <cokesboy@gmail.com>
2013-09-15 03:06:41 -0400

    Fix invalid logic

--- get_methods.rb
+++ get_methods.rb
@@ -1,15 +1,12 @@
 #!/usr/bin/env ruby
 # -*- coding: utf-8 :mode ruby-mode -*-
 
-def get_classes()
-  classes = Class.constants.select {|c| RbConfig.const_get(c).is_a? Class}
-  classes
-end
-
 def print_methods()
-  class_list = get_classes()
-  class_list.each do |krass|
-    krass.methods.each {|method| puts krass.to_s + "#" + method.to_s}
+  Class.constants.select do |c|
+    if Class.const_get(c).is_a? Class
+      klass = Class.const_get(c)
+      klass.methods.each {|method| puts klass.to_s + "#" + method.to_s}
+    end
   end
 end
 
